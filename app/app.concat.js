d78e5476fd9fa425a4f1210c5b8f9dbfdf77b8f1
Matheus Mantovani Bordin <Matheus Mantovani Bordin>
2016-11-30 14:02:43 -0200

    Make better

--- app/app.concat.js
+++ app/app.concat.js
@@ -54,6 +54,42 @@ angular
         },1000);
       }
 
+      $scope.init = function(){
+
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, 1000);
+              $("#bs-example-navbar-collapse-1").collapse('hide');
+              return false;
+            }
+          }
+        });
+
+        if(helper.getDevice == 'sm'){
+
+          $('#myLinks li a').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                $("#bs-example-navbar-collapse-1").collapse('hide');
+                return false;
+              }
+            }
+          });
+
+        }
+
+      }
+
     });
 angular
     .module('skynar')
